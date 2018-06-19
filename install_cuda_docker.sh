59e5f4f444ff07daf67063b466ccd75e0ac8b748
root <root@opnw3.aus.stglabs.ibm.com>
2018-06-19 18:22:49 -0500

    modified install sh for ppc64le

--- install_cuda_docker.sh
+++ install_cuda_docker.sh
@@ -1,20 +1,16 @@
 # Install CUDA
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt update
-sudo apt upgrade -y
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/ppc64el/cuda-repo-ubuntu1604_9.1.85-1_ppc64el.deb
+dpkg -i cuda-repo-ubuntu1604_9.1.85-1_ppc64el.deb
+apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/ppc64el/7fa2af80.pub
+sudo apt update && apt upgrade -y
 sudo apt install cuda -y
 
 # Install docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update
-sudo apt install docker-ce -y
+sudo add-apt-repository  "deb https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update && apt install docker-ce -y
 
 # Install nvidia-docker2
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
